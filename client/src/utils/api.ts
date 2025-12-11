4c34322f4ae36f41146e2b9384e46add972b047f
Prateek Jain <160221147+Prateekj2004@users.noreply.github.com>
2025-12-11 18:46:22 +0530

    Update api.ts

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://celestia-backend-q1s2.onrender.com/api",
   headers: {
     "Content-Type": "application/json"
   }
